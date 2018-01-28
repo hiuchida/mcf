9198c7da33a6a63cb87f4553aa1df97b4e353252
hiuchida <hiro4uchida@gmail.com>
2018-01-28 09:32:26 +0900

    junit

---
+++ mcflib/testsrc/mcflib/parser/ListParserTest.java
@@ -0,0 +1,53 @@
+package mcflib.parser;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import mcflib.model.History;
+import mcflib.model.HistoryBody;
+import mcflib.model.HistoryList;
+
+public class ListParserTest {
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetTimer() throws Exception {
+		List<String> list;
+		{
+			HistoryList hl = new HistoryList();
+			HistoryBody hb = new HistoryBody();
+			History h = new History(hl.getLastId(), hb);
+			hl.add(h);
+			
+			list = hl.toList();
+			TestCase.assertEquals(15, list.size());
+		}
+		{
+			HistoryList hl = (HistoryList)ListParser.parse(list);
+			TestCase.assertEquals(1, hl.getList().size());
+			
+			HistoryBody hb = new HistoryBody();
+			History h = new History(hl.getLastId(), hb);
+			hl.add(h);
+			hl.complete(null);
+			
+			list = hl.toList();
+			TestCase.assertEquals(26, list.size());
+		}
+		{
+			HistoryList hl = (HistoryList)ListParser.parse(list);
+			TestCase.assertEquals(2, hl.getList().size());
+		}
+	}
+
+}
