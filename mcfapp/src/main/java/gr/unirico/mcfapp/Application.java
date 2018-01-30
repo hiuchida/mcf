f8573d6e5cb8ebe2a24885b411a0ae4f7e694a86
hiuchida <hiro4uchida@gmail.com>
2018-01-30 20:07:34 +0900

    McfApiの保存先を/var/data/mcfappに設定

--- mcfapp/src/main/java/gr/unirico/mcfapp/Application.java
+++ mcfapp/src/main/java/gr/unirico/mcfapp/Application.java
@@ -3,9 +3,12 @@ package gr.unirico.mcfapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import gr.unirico.mcflib.api.McfApi;
+
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
+		McfApi.init("/var/data/mcfapp");
 		SpringApplication.run(Application.class, args);
 	}
 
