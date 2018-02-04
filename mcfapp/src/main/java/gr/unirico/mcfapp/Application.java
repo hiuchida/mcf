a64ca3c51e29e836adf788f5bb52758c0325fd9e
hiuchida <hiro4uchida@gmail.com>
2018-02-04 20:31:53 +0900

    起動時にdataフォルダのファイルを消す

--- mcfapp/src/main/java/gr/unirico/mcfapp/Application.java
+++ mcfapp/src/main/java/gr/unirico/mcfapp/Application.java
@@ -3,12 +3,17 @@ package gr.unirico.mcfapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import gr.unirico.mcflib.api.McfApi;
 import gr.unirico.mcflib.api.McfApiFactory;
+import gr.unirico.mcflib.util.FileUtil;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		McfApiFactory.init("/var/data/mcfapp");
+		McfApi api = McfApiFactory.getInstance();
+		String dir = api.getDataDir();
+		FileUtil.deleteFiles(dir);
 		SpringApplication.run(Application.class, args);
 	}
 
