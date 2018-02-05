7c7bdb573a32dc2e4134e48cd896a56fc6f4f1a1
hiuchida <hiro4uchida@gmail.com>
2018-02-05 09:56:06 +0900

    デモデータの登録

--- mcfapp/src/main/java/gr/unirico/mcfapp/Application.java
+++ mcfapp/src/main/java/gr/unirico/mcfapp/Application.java
@@ -1,10 +1,13 @@
 package gr.unirico.mcfapp;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import gr.unirico.mcflib.api.McfApi;
 import gr.unirico.mcflib.api.McfApiFactory;
+import gr.unirico.mcflib.api.Topic;
 import gr.unirico.mcflib.util.FileUtil;
 
 @SpringBootApplication
@@ -14,6 +17,20 @@ public class Application {
 		McfApi api = McfApiFactory.getInstance();
 		String dir = api.getDataDir();
 		FileUtil.deleteFiles(dir);
+		try {
+			Topic t;
+			t = api.newTopic("Yahoo!");
+			t.setUrl("https://www.yahoo.co.jp/");
+			api.writeTopic(t);
+			t = api.newTopic("Google");
+			t.setUrl("https://www.google.co.jp/");
+			api.writeTopic(t);
+			t = api.newTopic("Microsoft");
+			t.setUrl("https://www.microsoft.com/ja-jp/");
+			api.writeTopic(t);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		SpringApplication.run(Application.class, args);
 	}
 
