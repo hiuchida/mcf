e066bb18966848525a8f2619e00fef1a9529f3a2
hiuchida <hiro4uchida@gmail.com>
2018-02-13 13:08:14 +0900

    shorthash, shortprevhashを追加

--- mcflib/src/main/java/gr/unirico/mcflib/api/Node.java
+++ mcflib/src/main/java/gr/unirico/mcflib/api/Node.java
@@ -4,11 +4,13 @@ public interface Node {
 	public Node getParent();
 	public String getPrevid();
 	public String getPrevhash();
+	public String getShortprevhash();
 	public String getId();
 	public String getName();
 	public String getTimestamp();
 	public String getStatus();
 	public int getProof();
 	public String getHash();
+	public String getShorthash();
 
 }
