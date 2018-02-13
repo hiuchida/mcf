16a692467c90e6c31071b374d91e628eaed89d91
corestrike <corestrike@gmail.com>
2018-02-13 13:13:12 +0900

    ログイン後にcommon.cssに遷移する問題の修正

--- mcfapp/src/main/java/gr/unirico/mcfapp/config/WebSecurityConfig.java
+++ mcfapp/src/main/java/gr/unirico/mcfapp/config/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/resources/*", "/api/**");
+		web.ignoring().antMatchers("/resources/**", "/api/**");
 	}
 
 	@Override
