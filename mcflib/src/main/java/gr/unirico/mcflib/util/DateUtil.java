1fcbeb24cac06a4696f94688c31063534c072196
hiuchida <hiro4uchida@gmail.com>
2018-02-13 17:51:53 +0900

    TimeZoneを指定

--- mcflib/src/main/java/gr/unirico/mcflib/util/DateUtil.java
+++ mcflib/src/main/java/gr/unirico/mcflib/util/DateUtil.java
@@ -2,14 +2,18 @@ package gr.unirico.mcflib.util;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class DateUtil {
+	private static TimeZone jst = TimeZone.getTimeZone("JST");
 	private static DateFormat format4timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.JAPAN);
 
 	public static String createTimestampStr() {
-		return createTimestampStr(new Date());
+		Calendar c = Calendar.getInstance(jst);
+		return createTimestampStr(c.getTime());
 	}
 
 	public static String createTimestampStr(Date date) {
